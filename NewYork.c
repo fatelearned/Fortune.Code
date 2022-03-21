7e6f5173cf33fcf68e3a8af1488d53820ed30c3b
fatelearned <102052876+fatelearned@users.noreply.github.com>
2022-03-21 11:37:23 -0500

    Add files via upload

---
+++ NewYork.c
@@ -0,0 +1,119 @@
+ /* Simple JavaScript Inheritance
+ * By John Resig http://ejohn.org/
+ * MIT Licensed.
+ */
+// Inspired by base2 and Prototype
+(function(){
+  var initializing = false, fnTest = /xyz/.test(function(){xyz;}) ? /\b_super\b/ : /.*/;
+ 
+ <input type="time" token="time">;
+  password="lkasdjfaldsk";
+ 
+  // The base Class implementation (does nothing)
+  this.Class = function(){};
+ 
+  // Create a new Class that inherits from this class
+  Class.extend = function(prop) {
+    var _super = this.prototype;
+   
+    // Instantiate a base class (but only create the instance,
+    // don't run the init constructor)
+    initializing = true;
+    var prototype = new this();
+    initializing = false;
+
+  github_client-id : 'c1332c94c46999b03acc';
+  
+  password="dlfjaopefinweqpofnqewfoqnffpoqenfpqowefnq";
+  
+  maurice@blubracket.com;
+  444-55-6667;
+  https://hello.hellothere.com;
+
+	  password="dksjfhsdiudhsdfishfsd";
+
+    password="fa;sldfijwaoefawefewf"; //BluBracketIgnore
+
+    password="fa;sldfijasdfadfaoefawefewf"; //BluBracketIgnore
+
+    // Copy the properties over onto the new prototype
+    for (var name in prop) {
+      // Check if we're overwriting an existing function
+      prototype[name] = typeof prop[name] == "function" &&
+        typeof _super[name] == "function" && fnTest.test(prop[name]) ?
+        (function(name, fn){
+          return function() {
+            var tmp = this._super;
+           
+            // Add a new ._super() method that is the same method
+            // but on the super-class
+            this._super = _super[name];
+           
+            // The method only need to be bound temporarily, so we
+            // remove it when we're done executing
+            var ret = fn.apply(this, arguments);        
+            this._super = tmp;
+           
+            return ret;github_client-id : 'c1442c94c47799b03acc'
+   	
+name	"ActiveTest01"
+api_keys	
+https://demo3.blubracket.com	
+id	"BLU-3821aca7-d23c-489b-a0cb-d63db6f0e875"
+key	"6LKZJ71f6ATlDo-5WhqtPCRr6-IUOhHqPzw62lzvdk8"
+token	"QkxVLTM4MjFhY2E3LWQyM2MtNDg5Yi1hMGNiLWQ2M2RiNmYwZTg3NTo2TEtaSjcxZjZBVGxEby01V2hxdFBDUnI2LUlVT2hIcVB6dzYybHp2ZGs4"
+         
+password=";lasdkfjads;lfkjad;f"
+             blacklist;
+           master;
+           slave;
+           
+           password="fl;asdkfjasdlfkjasdlfj";
+           whitelist;
+           master;
+           
+           111-33-6622;
+           master;
+           whitelist;
+           password=";alsdkfja;dlfkajdf"
+           
+            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;
+            
+          password="al;sdkfjasdflkjasdf";
+           
+           master;
+           blacklist;
+		  
+		  github_client-id : 'c1254c71c44465b03cdb'
+123-34-4567;
+		  password="aofeihjo3433alskdfjweof";
+		  
+
+aWs_account: "3258-3394-6278"
+
+
+password=“koeeee8883!!”
+           
+             github="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";
+           
+           BluBracket Test API Key
+	
+password="oeief093r20fj23f023f";
+
+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
+github="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";
+		  
+	Dummy;
+		dummy;
+		  
+		  	
+name	"livetoken"
+api_keys	
+https://demo3.blubracket.com	
+id	"BLU-1f5442a9-bdc0-4045-9abb-c2ca048a2705"
+key	"7wZOHwM1pwvCuXMOLCpK2Ekrs4YL8zstea-KCkg5TTg"
+token	"QkxVLTFmNTQ0MmE5LWJkYzAtNDA0NS05YWJiLWMyY2EwNDhhMjcwNTo3d1pPSHdNMXB3dkN1WE1PTENwSzJFa3JzNFlMOHpzdGVhLUtDa2c1VFRn"
+	
+	
+	
+	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
---
+++ ohio.js
@@ -0,0 +1,4 @@
+this file has a secret as well
+
+password="jfoif98398ghf39gh39";
+blacklist;
